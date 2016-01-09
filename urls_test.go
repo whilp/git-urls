87570d7bfbc0785e2b52135a5dcd1542af84cc46
Will Maier <wcmaier@gmail.com>
2016-01-09 20:56:08 +0000

    Compare URLs directly in tests
    Reveals that we're not setting User consistently. Paper over this in
    tests for now.

--- urls_test.go
+++ urls_test.go
@@ -9,142 +9,121 @@ import (
 var tests []*Test
 
 type Test struct {
-	rawurl string
-	result *Result
+	in   string
+	want *url.URL
 }
 
-type Result struct {
-	Transport string
-	User      string
-	Host      string
-	Path      string
-}
-
-func NewResult(transport, user, host, path string) *Result {
-	return &Result{
-		Transport: transport,
-		User:      user,
-		Host:      host,
-		Path:      path,
+func NewTest(in, transport, user, host, path string) *Test {
+	return &Test{
+		in: in,
+		want: &url.URL{
+			Scheme: transport,
+			Host:   host,
+			Path:   path,
+			User:   url.User(user),
+		},
 	}
-}
-
-func ResultFromURL(u *url.URL) *Result {
-	var user string
-	if u.User == nil {
-		user = ""
-	} else {
-		user = u.User.String()
-	}
-	return NewResult(
-		u.Scheme,
-		user,
-		u.Host,
-		u.Path,
-	)
 }
 
 func init() {
 	// https://www.kernel.org/pub/software/scm/git/docs/git-clone.html
 	tests = []*Test{
-		&Test{
+		NewTest(
 			"user@host.xz:path/to/repo.git/",
-			NewResult("ssh", "user", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ssh", "user", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"host.xz:path/to/repo.git/",
-			NewResult("ssh", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ssh", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"host.xz:path/to/repo-with_specials.git/",
-			NewResult("ssh", "", "host.xz", "/path/to/repo-with_specials.git/"),
-		},
-		&Test{
+			"ssh", "", "host.xz", "/path/to/repo-with_specials.git/",
+		),
+		NewTest(
 			"git://host.xz/path/to/repo.git/",
-			NewResult("git", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"git", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"git://host.xz:1234/path/to/repo.git/",
-			NewResult("git", "", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"git", "", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"http://host.xz/path/to/repo.git/",
-			NewResult("http", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"http", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"http://host.xz:1234/path/to/repo.git/",
-			NewResult("http", "", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"http", "", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"https://host.xz/path/to/repo.git/",
-			NewResult("https", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"https", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"https://host.xz:1234/path/to/repo.git/",
-			NewResult("https", "", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"https", "", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ftp://host.xz/path/to/repo.git/",
-			NewResult("ftp", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ftp", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ftp://host.xz:1234/path/to/repo.git/",
-			NewResult("ftp", "", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ftp", "", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ftps://host.xz/path/to/repo.git/",
-			NewResult("ftps", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ftps", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ftps://host.xz:1234/path/to/repo.git/",
-			NewResult("ftps", "", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ftps", "", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"rsync://host.xz/path/to/repo.git/",
-			NewResult("rsync", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"rsync", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ssh://user@host.xz:1234/path/to/repo.git/",
-			NewResult("ssh", "user", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ssh", "user", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ssh://host.xz:1234/path/to/repo.git/",
-			NewResult("ssh", "", "host.xz:1234", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ssh", "", "host.xz:1234", "/path/to/repo.git/",
+		),
+		NewTest(
 			"ssh://host.xz/path/to/repo.git/",
-			NewResult("ssh", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"ssh", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"git+ssh://host.xz/path/to/repo.git/",
-			NewResult("git+ssh", "", "host.xz", "/path/to/repo.git/"),
-		},
-		&Test{
+			"git+ssh", "", "host.xz", "/path/to/repo.git/",
+		),
+		NewTest(
 			"/path/to/repo.git/",
-			NewResult("file", "", "", "/path/to/repo.git/"),
-		},
-		&Test{
+			"file", "", "", "/path/to/repo.git/",
+		),
+		NewTest(
 			"file:///path/to/repo.git/",
-			NewResult("file", "", "", "/path/to/repo.git/"),
-		},
+			"file", "", "", "/path/to/repo.git/",
+		),
 	}
 }
 
 func TestParse(t *testing.T) {
 	for _, tt := range tests {
-		url, err := Parse(tt.rawurl)
-		if url == nil {
-			t.Errorf("Parse(%q) -> nil", tt.rawurl)
+		got, err := Parse(tt.in)
+		if err != nil {
+			t.Errorf("Parse(%q) = unexpected err %q, want %q", tt.in, err, tt.want)
 			continue
 		}
-		if err != nil {
-			t.Errorf("Parse(%q) -> unexpected err %q", tt.rawurl, err)
-			continue
+		if got.User == nil {
+			got.User = url.User("")
 		}
 
-		r := ResultFromURL(url)
-		if !reflect.DeepEqual(r, tt.result) {
-			t.Errorf("Parse(%q) -> %q, want %q", tt.rawurl, r, tt.result)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("Parse(%q) = %q, want %q", tt.in, got, tt.want)
 		}
 	}
 }
